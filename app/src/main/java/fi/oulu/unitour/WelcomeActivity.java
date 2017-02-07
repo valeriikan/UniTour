9cbdf8f5a8e6141b3e8e698678214a3aedd4a0e6
ValeriiKan <valerii.kan1@gmail.com>
2017-02-07 12:37:33 +0200

    Welcome AlertDialog added

--- app/src/main/java/fi/oulu/unitour/WelcomeActivity.java
+++ app/src/main/java/fi/oulu/unitour/WelcomeActivity.java
@@ -1,5 +1,7 @@
 package fi.oulu.unitour;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,17 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        AlertDialog welcomeDialog = new AlertDialog.Builder(this).create();
+        welcomeDialog.setTitle("Welcome");
+        welcomeDialog.setMessage("This application is created to help you to get acknowledged with the university");
+        welcomeDialog.setButton(AlertDialog.BUTTON_POSITIVE, "Got it", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        welcomeDialog.show();
+
     }
 }
