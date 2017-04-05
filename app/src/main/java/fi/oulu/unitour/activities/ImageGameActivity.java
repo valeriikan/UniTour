1ab031691609025cdd8ebc12e31d6b8d0ae3e51b
ValeriiKan <valerii.kan1@gmail.com>
2017-04-05 23:26:13 +0300

    QuestActivity and ImageGameActivity content loading changed from web to local

--- app/src/main/java/fi/oulu/unitour/activities/ImageGameActivity.java
+++ app/src/main/java/fi/oulu/unitour/activities/ImageGameActivity.java
@@ -6,36 +6,27 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.squareup.picasso.Callback;
-import com.squareup.picasso.Picasso;
 
 import fi.oulu.unitour.R;
-import fi.oulu.unitour.helpers.RoundedCornersTransform;
 
 public class ImageGameActivity extends AppCompatActivity {
 
+    //declaration of variables for layout elements
     ImageView img1, img2, img3, img4, frame1, frame2, frame3, frame4;
-    ProgressBar img1Loading,img2Loading,img3Loading,img4Loading;
     TextView question;
     Button imageButton;
 
-    String imageUrl1;
-    String imageUrl2;
-    String imageUrl3;
-    String imageUrl4;
-
-    int answer = 0;
-
     //Firebase authentication object
     FirebaseAuth mAuth;
-    DatabaseReference mDatabase;
+    DatabaseReference mRefference;
+
+    int answer = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,13 +34,13 @@ public class ImageGameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_image_game);
 
         String placeId = getIntent().getStringExtra("LOCATION_ID");
-
         //Firebase elements declaration
         mAuth = FirebaseAuth.getInstance();
-        mDatabase = FirebaseDatabase.getInstance().getReference().child("users");
         String userId = mAuth.getCurrentUser().getUid();
-        final DatabaseReference locRefference = mDatabase.child(userId).child("gamedata").child("loc"+placeId);
+        mRefference = FirebaseDatabase.getInstance().getReference()
+                .child("users").child(userId).child("gamedata").child("loc"+placeId);
 
+        //attaching layout elements to variables
         img1 = (ImageView) findViewById(R.id.img1);
         img2 = (ImageView) findViewById(R.id.img2);
         img3 = (ImageView) findViewById(R.id.img3);
@@ -62,79 +53,24 @@ public class ImageGameActivity extends AppCompatActivity {
         frame2.setImageResource(R.drawable.view_frame);
         frame3.setImageResource(R.drawable.view_frame);
         frame4.setImageResource(R.drawable.view_frame);
-        img1Loading = (ProgressBar) findViewById(R.id.img1Loading);
-        img2Loading = (ProgressBar) findViewById(R.id.img2Loading);
-        img3Loading = (ProgressBar) findViewById(R.id.img3Loading);
-        img4Loading = (ProgressBar) findViewById(R.id.img4Loading);
         question = (TextView) findViewById(R.id.question);
         imageButton = (Button) findViewById(R.id.imageButton);
 
-
-
         if (placeId.equals("1")) {
-            imageUrl1 = "https://firebasestorage.googleapis.com/v0/b/unitour-7b1ed.appspot.com/o/gamepictures%2Fkastari1.JPG?alt=media&token=3cf26172-61f3-42bf-94d9-c788f8d7e2fb";
-            imageUrl2 = "https://firebasestorage.googleapis.com/v0/b/unitour-7b1ed.appspot.com/o/gamepictures%2Fkastari2.JPG?alt=media&token=fe9fc6e2-4448-4aa5-900c-4c1cd48fec94";
-            imageUrl3 = "https://firebasestorage.googleapis.com/v0/b/unitour-7b1ed.appspot.com/o/gamepictures%2Fkastari3.JPG?alt=media&token=6f3cc41d-8e9d-4fe6-9cb4-0e7fe2ac5d78";
-            imageUrl4 = "https://firebasestorage.googleapis.com/v0/b/unitour-7b1ed.appspot.com/o/gamepictures%2Fkastari4.jpg?alt=media&token=9806ffc8-e3ce-4bed-82fa-3e252fafdbd3";
-            question.setText("Which artwork is in Kastari");
+            img1.setImageResource(R.drawable.game_1_1);
+            img2.setImageResource(R.drawable.game_1_2);
+            img3.setImageResource(R.drawable.game_1_3);
+            img4.setImageResource(R.drawable.game_1_4);
+            question.setText("Which artwork is in Kastari?");
         }
 
         if (placeId.equals("13")) {
-            imageUrl1 = "https://firebasestorage.googleapis.com/v0/b/unitour-7b1ed.appspot.com/o/gamepictures%2Fmuseum1.JPG?alt=media&token=2fed7d4d-6902-4751-bc5b-5e94bb0880fe";
-            imageUrl2 = "https://firebasestorage.googleapis.com/v0/b/unitour-7b1ed.appspot.com/o/gamepictures%2Fmuseum2.JPG?alt=media&token=dad721b5-181a-45ef-864a-02205c585820";
-            imageUrl3 = "https://firebasestorage.googleapis.com/v0/b/unitour-7b1ed.appspot.com/o/gamepictures%2Fmuseum3.JPG?alt=media&token=833d49ac-d39b-4488-97ba-39eceb84af98";
-            imageUrl4 = "https://firebasestorage.googleapis.com/v0/b/unitour-7b1ed.appspot.com/o/gamepictures%2Fmuseum4.jpg?alt=media&token=7519181c-9cf4-40da-9de8-5215984b0315";
+            img1.setImageResource(R.drawable.game_13_1);
+            img2.setImageResource(R.drawable.game_13_2);
+            img3.setImageResource(R.drawable.game_13_3);
+            img4.setImageResource(R.drawable.game_13_4);
             question.setText("Which of the following animals doesn’t belong to the Zoological Museum?");
         }
-
-        Callback callback1 = new Callback() {
-            @Override
-            public void onSuccess() {
-                img1Loading.setVisibility(View.GONE);
-            }
-
-            @Override
-            public void onError() {
-            }
-        };
-
-        Callback callback2 = new Callback() {
-            @Override
-            public void onSuccess() {
-                img2Loading.setVisibility(View.GONE);
-            }
-
-            @Override
-            public void onError() {
-            }
-        };
-
-        Callback callback3 = new Callback() {
-            @Override
-            public void onSuccess() {
-                img3Loading.setVisibility(View.GONE);
-            }
-
-            @Override
-            public void onError() {
-            }
-        };
-
-        Callback callback4 = new Callback() {
-            @Override
-            public void onSuccess() {
-                img4Loading.setVisibility(View.GONE);
-            }
-
-            @Override
-            public void onError() {
-            }
-        };
-
-        Picasso.with(ImageGameActivity.this).load(imageUrl1).fit().centerCrop().transform(new RoundedCornersTransform()).into(img1, callback1);
-        Picasso.with(ImageGameActivity.this).load(imageUrl2).fit().centerCrop().transform(new RoundedCornersTransform()).into(img2, callback2);
-        Picasso.with(ImageGameActivity.this).load(imageUrl3).fit().centerCrop().transform(new RoundedCornersTransform()).into(img3, callback3);
-        Picasso.with(ImageGameActivity.this).load(imageUrl4).fit().centerCrop().transform(new RoundedCornersTransform()).into(img4, callback4);
 
         img1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -144,7 +80,6 @@ public class ImageGameActivity extends AppCompatActivity {
                 frame3.setVisibility(View.INVISIBLE);
                 frame4.setVisibility(View.INVISIBLE);
                 answer = 1;
-                locRefference.setValue("1");
             }
         });
 
@@ -156,7 +91,6 @@ public class ImageGameActivity extends AppCompatActivity {
                 frame3.setVisibility(View.INVISIBLE);
                 frame4.setVisibility(View.INVISIBLE);
                 answer = 2;
-                locRefference.setValue("1");
             }
         });
 
@@ -168,7 +102,6 @@ public class ImageGameActivity extends AppCompatActivity {
                 frame3.setVisibility(View.VISIBLE);
                 frame4.setVisibility(View.INVISIBLE);
                 answer = 3;
-                locRefference.setValue("1");
             }
         });
 
@@ -180,36 +113,28 @@ public class ImageGameActivity extends AppCompatActivity {
                 frame3.setVisibility(View.INVISIBLE);
                 frame4.setVisibility(View.VISIBLE);
                 answer = 4;
-                locRefference.setValue("1");
             }
         });
 
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (answer==0) {
-                    Toast.makeText(ImageGameActivity.this, "Select an answer", Toast.LENGTH_LONG).show();
-
-                }
-
                 switch (answer) {
                     case 0:
                         Toast.makeText(ImageGameActivity.this, "Select an answer", Toast.LENGTH_LONG).show();
                         break;
                     case 1: case 2: case 3:
-                        Intent map123 = new Intent(ImageGameActivity.this, QuestMapActivity.class);
-                        startActivity(map123);
-                        Toast.makeText(ImageGameActivity.this, "Wrong answer. You do not receive UniTour point", Toast.LENGTH_LONG).show();
+                        Toast.makeText(ImageGameActivity.this, "Wrong answer. Try again", Toast.LENGTH_LONG).show();
                         break;
                     case 4:
-                        Intent map4 = new Intent(ImageGameActivity.this, QuestMapActivity.class);
-                        startActivity(map4);
+                        mRefference.setValue("1");
+                        Intent map = new Intent(ImageGameActivity.this, QuestMapActivity.class);
+                        startActivity(map);
                         Toast.makeText(ImageGameActivity.this, "Correct answer! You gained 1 UniTour point", Toast.LENGTH_LONG).show();
                         break;
                 }
-
             }
         });
-
     }
+
 }
--- app/src/main/java/fi/oulu/unitour/activities/QuestActivity.java
+++ app/src/main/java/fi/oulu/unitour/activities/QuestActivity.java
@@ -6,18 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
 import android.widget.TextView;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.squareup.picasso.Callback;
-import com.squareup.picasso.Picasso;
-
-import java.util.Map;
 
 import fi.oulu.unitour.R;
 
@@ -26,25 +15,33 @@ public class QuestActivity extends AppCompatActivity {
     //declaration of variables for layout elements
     ImageView imgQuest;
     TextView tvQuest;
-    ProgressBar loadingBarQuest;
     Button btnScanQR;
-
-    //Firebase authentication objects
-    DatabaseReference mDatabase;
+    String placeId;
 
     @Override
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
         setContentView(R.layout.activity_quest);
 
-        final String placeId = getIntent().getStringExtra("LOCATION_ID");
-
         //attaching layout elements to variables
         imgQuest = (ImageView) findViewById(R.id.imgQuest);
         tvQuest = (TextView) findViewById(R.id.tvQuest);
         btnScanQR = (Button) findViewById(R.id.btnScanQR);
-        loadingBarQuest = (ProgressBar) findViewById(R.id.loadingBarQuest);
 
+        //getting values according to place id
+        placeId = getIntent().getStringExtra("LOCATION_ID");
+        int titleId = getResources().getIdentifier("loc_name_" + placeId, "string", getPackageName());
+        int descriptionId = getResources().getIdentifier("loc_description_" + placeId, "string", getPackageName());
+        int imgId = getResources().getIdentifier("location_" + placeId, "drawable", getPackageName());
+        String description = getString(descriptionId);
+        String title = getString(titleId);
+
+        //applying values to layout elements
+        setTitle(title);
+        tvQuest.setText(description);
+        imgQuest.setImageResource(imgId);
+
+        //scan button
         btnScanQR.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -53,39 +50,5 @@ public class QuestActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-        //retrieving place data from Firebase database
-        //String placeId = getIntent().getStringExtra("LOCATION_ID");
-        mDatabase = FirebaseDatabase.getInstance().getReference().child("locations").child(placeId);
-        mDatabase.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                Map<String, String> map = (Map) dataSnapshot.getValue();
-                String name = map.get("name");
-                String description = map.get("description");
-                String imageUrl = map.get("imageUrl");
-                setTitle(name);
-                tvQuest.setText(description);
-
-                Callback loadingCallback = new Callback() {
-                    @Override
-                    public void onSuccess() {
-                        loadingBarQuest.setVisibility(View.GONE);
-                    }
-
-                    @Override
-                    public void onError() {
-                    }
-                };
-
-                Picasso.with(QuestActivity.this).load(imageUrl).fit().centerCrop().into(imgQuest, loadingCallback);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
-
     }
 }
